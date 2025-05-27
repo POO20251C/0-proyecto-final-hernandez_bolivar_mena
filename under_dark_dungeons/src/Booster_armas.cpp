4ee0229d50b1212d64c5451703f71362a0c783d7
J-p-M77 <131775741+J-p-M77@users.noreply.github.com>
2025-05-27 02:30:46 -0500

    Update Booster_armas.cpp

--- under_dark_dungeons/src/Booster_armas.cpp
+++ under_dark_dungeons/src/Booster_armas.cpp
@@ -1,6 +1,3 @@
-//
-// Created by juanj on 07/05/2025.
-//
 #include "Booster_armas.h"
 #include "Efecto.h"
 #include <iostream>
@@ -10,21 +7,20 @@
 Booster_armas::Booster_armas() {
     nombre = "Booster de Armas";
 
-    armas.push_back(Arma("Espada de fuego", Efecto::Quemadura(), 50, 1, 1));
-    armas.push_back(Arma("Hacha de hielo", Efecto::Congelacion(), 45, 1, 1));
-    armas.push_back(Arma("Lanza venenosa", Efecto::Veneno(), 40, 1, 1));
-    armas.push_back(Arma("Arco eléctrico", Efecto::Paralisis(), 35, 1, 1));
-    armas.push_back(Arma("Martillo de trueno", Efecto::Electrocucion(), 55, 1, 1));
-    armas.push_back(Arma("Daga sombría", Efecto::Veneno(), 30, 1, 1));
-    armas.push_back(Arma("Báculo de luz", Efecto::Quemadura(), 40, 1, 1));
-    armas.push_back(Arma("Espada helada", Efecto::Congelacion(), 48, 1, 1));
-    armas.push_back(Arma("Lanza de plasma", Efecto::Paralisis(), 52, 1, 1));
-
+    armas.push_back(Arma("Espada de fuego", Efecto::Quemadura(), 50, 1, 1, 150));
+    armas.push_back(Arma("Hacha de hielo", Efecto::Congelacion(), 45, 1, 1, 140));
+    armas.push_back(Arma("Lanza venenosa", Efecto::Veneno(), 40, 1, 1, 130));
+    armas.push_back(Arma("Arco eléctrico", Efecto::Paralisis(), 35, 1, 1, 125));
+    armas.push_back(Arma("Martillo de trueno", Efecto::Electrocucion(), 55, 1, 1, 160));
+    armas.push_back(Arma("Daga sombría", Efecto::Veneno(), 30, 1, 1, 110));
+    armas.push_back(Arma("Báculo de luz", Efecto::Quemadura(), 40, 1, 1, 135));
+    armas.push_back(Arma("Espada helada", Efecto::Congelacion(), 48, 1, 1, 145));
+    armas.push_back(Arma("Lanza de plasma", Efecto::Paralisis(), 52, 1, 1, 155));
 }
 
 void Booster_armas::abrir() {
 
-
+    
     static bool seed_inicializada = false;
     if (!seed_inicializada) {
         std::srand(static_cast<unsigned>(std::time(nullptr)));
@@ -35,6 +31,7 @@ void Booster_armas::abrir() {
     Arma arma_obtenida = armas[i];
 
     std::cout << "Has obtenido: " << arma_obtenida.getName()
-              << " Efecto: " << arma_obtenida.getEfecto().getname()
-              << "Ataque: " << arma_obtenida.getAtk() << "\n";
+              << " | Efecto: " << arma_obtenida.getEfecto().getname()
+              << " | Ataque: " << arma_obtenida.getAtk()
+              << " | Precio: " << arma_obtenida.getPrecio() << "\n";
 }
