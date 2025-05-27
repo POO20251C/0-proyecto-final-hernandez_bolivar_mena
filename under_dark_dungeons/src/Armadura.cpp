23febe2f245acab5571dd392583ed6c7676f32c9
J-p-M77 <131775741+J-p-M77@users.noreply.github.com>
2025-05-27 02:18:06 -0500

    Update Armadura.cpp

--- under_dark_dungeons/src/Armadura.cpp
+++ under_dark_dungeons/src/Armadura.cpp
@@ -1,25 +1,27 @@
 #include "Armadura.h"
 #include <algorithm>
 
-Armadura::Armadura(std::string name, int def, int vel, std::vector<Efecto> inv) {
-
-	this->name = name;
-	this->def_p = def;
-	this->vel_n = vel;
-	this->invulnerable = inv;
-
-};
+Armadura::Armadura(std::string name, int def, int vel, std::vector<Efecto> inv, int precio) {
+    this->name = name;
+    this->def_p = def;
+    this->vel_n = vel;
+    this->invulnerable = inv;
+    this->precio = precio; 
+}
 
 
 std::string Armadura::getName() {
-	return this->name;
+    return this->name;
 }
 
 int Armadura::getDef() {
-	return this->def_p;
+    return this->def_p;
 }
 
 int Armadura::getVel() {
-	return this->vel_n;
+    return this->vel_n;
 }
 
+int Armadura::getPrecio() {
+    return this->precio;
+}
