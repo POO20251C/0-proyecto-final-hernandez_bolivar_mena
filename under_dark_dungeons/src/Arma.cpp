5ac4b10ea9a5bf99c40cba2ea15a0b24a9b6e17b
J-p-M77 <131775741+J-p-M77@users.noreply.github.com>
2025-05-27 02:15:56 -0500

    Update Arma.cpp

--- under_dark_dungeons/src/Arma.cpp
+++ under_dark_dungeons/src/Arma.cpp
@@ -1,45 +1,48 @@
 #include "Arma.h"
 #include "Efecto.h"
 
-// Constructores
 
-Arma::Arma(std::string name, Efecto efecto, int atk, int des, int lck) : name(name), efecto(efecto), atk_p(atk), des_p(des), lck_p(lck) {}
+Arma::Arma(std::string name, Efecto efecto, int atk, int des, int lck, int precio)
+    : name(name), efecto(efecto), atk_p(atk), des_p(des), lck_p(lck), precio(precio) {}
 
 Arma* Arma::Espada_basica() {
-	return new Arma("Espada basica", Efecto::Nada(), 15, -2, 0);
+    return new Arma("Espada basica", Efecto::Nada(), 15, -2, 0, 100);
 }
 
 Arma* Arma::Baston_basico() {
-	return new Arma("Baston magico debil", Efecto::Nada(), 5,  0, 5);
+    return new Arma("Baston magico debil", Efecto::Nada(), 5,  0, 5, 80);
 }
 
 Arma* Arma::Daga_basica() {
-	return new Arma("Daga de iniciacion", Efecto::Nada(), 10, 10, 5);
+    return new Arma("Daga de iniciacion", Efecto::Nada(), 10, 10, 5, 90);
 }
 
 Arma* Arma::Palo_desgastado() {
-	return new Arma("Palo desgastado", Efecto::Nada(), 7, 1, 1);
+    return new Arma("Palo desgastado", Efecto::Nada(), 7, 1, 1, 70);
 }
 
 
-// Getters
- 
+
 std::string Arma::getName() const {
-	return name;
+    return name;
 }
 
 Efecto Arma::getEfecto() const {
-	return efecto;
+    return efecto;
 }
 
 int Arma::getAtk() const {
-	return atk_p;
+    return atk_p;
 }
 
 int Arma::getDes() const {
-	return this->des_p;
+    return des_p;
 }
 
 int Arma::getLck() const {
-	return this->lck_p;
+    return lck_p;
+}
+
+int Arma::getPrecio() const {
+    return precio;
 }
