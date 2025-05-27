93a0d3ed902137d7507e8fac2812cebba9b13c3d
J-p-M77 <131775741+J-p-M77@users.noreply.github.com>
2025-05-27 02:06:37 -0500

    Update Grupo_jugador.cpp

--- under_dark_dungeons/src/Grupo_jugador.cpp
+++ under_dark_dungeons/src/Grupo_jugador.cpp
@@ -1,79 +1,71 @@
 #include "Grupo_jugador.h"
 #include <string>
 
-// Constructor
-
 GrupoJugador::GrupoJugador(std::string name, int dano_t, int oro) {
-	this->name = name;
-	this->dano_t = dano_t;
-	this->oro = oro;
+    this->name = name;
+    this->dano_t = dano_t;
+    this->oro = oro;
 }
 
-
-// Getters
-
 std::string GrupoJugador::getName() {
-	return this->name;
+    return this->name;
 }
 
 int GrupoJugador::getOro() {
-	return this->oro;
+    return this->oro;
 }
 
 vector<Heroe*> GrupoJugador::getHeroes() {
-	return this->heroes;
+    return this->heroes;
 }
 
 vector<Objeto*> GrupoJugador::getInventario() {
-	return this->inventario;
+    return this->inventario;
 }
 
 vector<Armadura*> GrupoJugador::getArmaduras() {
-	return this->armaduras;
+    return this->armaduras;
 }
 
 vector<Arma*> GrupoJugador::getArmas() {
-	return this->armas;
+    return this->armas;
 }
 
-// Setters
 void GrupoJugador::setHeroes(vector<Heroe*> grupo) {
-	this->heroes = grupo;
+    this->heroes = grupo;
 }
 
-void GrupoJugador::sumarDano_t(int dnao) {
-	this->dano_t += dnao;
+void GrupoJugador::sumarDano_t(int dano) {
+    this->dano_t += dano;
 }
 
-std::string  GrupoJugador::sumarOro(int oro) {
-
-	std::string ans = "El grupo gano " + to_string(oro) + " de oro.";
-	this->oro += oro;
-
-	return ans;
+std::string GrupoJugador::sumarOro(int cantidad) {
+    if (cantidad < 0 && oro + cantidad < 0) {
+        return "No tienes suficiente oro.";
+    }
+    oro += cantidad;
+    if (cantidad >= 0) {
+        return "El grupo ganó " + std::to_string(cantidad) + " de oro.";
+    } else {
+        return "Se gastaron " + std::to_string(-cantidad) + " de oro.";
+    }
 }
 
-
-std::string GrupoJugador::usarItem(int indx, Heroe* heroe) 
-{
-
-	std::string ans = "Se uso " + inventario[indx]->getName() + " en " + heroe->nameGetter();
-	return ans;	
+std::string GrupoJugador::usarItem(int indx, Heroe* heroe) {
+    std::string ans = "Se uso " + inventario[indx]->getName() + " en " + heroe->nameGetter();
+    return ans;    
 }
 
-std::string GrupoJugador::equiparArma(int indx, Heroe* heroe) 
-{
-	std::string ans = "Se equipo " + armas[indx]->getName() + " a " + heroe->nameGetter();
-	return ans;
+std::string GrupoJugador::equiparArma(int indx, Heroe* heroe) {
+    std::string ans = "Se equipo " + armas[indx]->getName() + " a " + heroe->nameGetter();
+    return ans;
 }
 
-
-std::string GrupoJugador::equiparArmadura(int indx, Heroe* heroe) 
-{
-	std::string ans = "Se equipo " + armaduras[indx]->getName() + " en " + heroe->nameGetter();
-	return ans;
+std::string GrupoJugador::equiparArmadura(int indx, Heroe* heroe) {
+    std::string ans = "Se equipo " + armaduras[indx]->getName() + " en " + heroe->nameGetter();
+    return ans;
 }
 
 bool GrupoJugador::getDerrota() {
-	return this->derrotado;
+    return this->derrotado;
 }
