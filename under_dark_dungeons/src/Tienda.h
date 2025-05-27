57124c5a9e577c86e903ecca716d421c5788a06d
J-p-M77 <131775741+J-p-M77@users.noreply.github.com>
2025-05-27 01:18:51 -0500

    Update Tienda.h

--- under_dark_dungeons/src/Tienda.h
+++ under_dark_dungeons/src/Tienda.h
@@ -1,40 +1,46 @@
 #ifndef TIENDA_H
 #define TIENDA_H
 
-#include "Arma.h"
-#include "Armadura.h"
-#include "Booster_armas.h"
-#include "Booster_armaduras.h"
 #include <vector>
-#include <random>
 #include <memory>
-
 using namespace std;
+
+class Arma;
+class Armadura;
+class Booster_armas;
+class Booster_armaduras;
+
+class Nivel {
+public:
+    int numero;
+    bool completado;
+    Nivel(int n) : numero(n), completado(false) {}
+};
 
 class Tienda {
 private:
-    vector<Arma*> armasOferta;
+    vector<Nivel> niveles;
+    vector<unique_ptr<Arma>> armasOferta;
     vector<Armadura> armadurasOferta;
     unique_ptr<Booster_armas> boosterArmas;
     unique_ptr<Booster_armaduras> boosterArmaduras;
     bool boosterElegido;
-    vector<Nivel> niveles;
 
-public:
-    Tienda();
-    ~Tienda();
-    
-    void iniciarNiveles(int cantidad);
-    bool marcarNivelComoTerminado(int nivel);
-    bool puedeEntrar();
-    
-    void abrirTienda(const vector<Arma*>& todasLasArmas, const vector<Armadura>& todasLasArmaduras);
-    void crearOfertas(const vector<Arma*>& todasLasArmas, const vector<Armadura>& todasLasArmaduras);
+    void crearOfertas(const vector<shared_ptr<Arma>>&, const vector<Armadura>&);
     void mostrarOfertas() const;
     void elegir();
     void elegirBooster();
     void comprar(int tipo, int cual);
     void aplicarBooster();
+
+public:
+    Tienda();
+    ~Tienda();
+
+    void iniciarNiveles(int cantidad);
+    bool marcarNivelComoTerminado(int nivel);
+    bool puedeEntrar();
+    void abrirTienda(const vector<shared_ptr<Arma>>&, const vector<Armadura>&);
 };
 
-#endif  // TIENDA_H
+#endif
